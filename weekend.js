e39bc9625f616b18e0193325d03b9e70b4027c67
Taylor Hill <ineedfree360s@aol.com>
2019-04-10 18:40:43 -0400

    total amount

--- weekend.js
+++ weekend.js
@@ -3,11 +3,12 @@ var spendings = document.getElementById("spendings")
 
 
 function addTo() {
-  var value = amount.value
-  var newSpending = document.createElement("ol")
-  newSpending.innerHTML = value
-  newSpending.onclick = clear
-  spendings.appendChild(newSpending)
+  var value = amount.value;
+  var newSpending = document.createElement("ol");
+  newSpending.innerHTML = value;
+  newSpending.onclick = clear;
+  spendings.appendChild(newSpending);
+  console.log(fruits.length);
 }
 
 function clear() {
