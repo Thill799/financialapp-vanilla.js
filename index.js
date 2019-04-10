a48e33920215c292a4ebb1cd1f65ecfa96124c6d
Taylor Hill <ineedfree360s@aol.com>
2019-04-10 19:49:09 -0400

    total amount

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var amount = document.getElementById("amount")
 var spendings = document.getElementById("spendings")
-
+var total = document.getElementById('total')
 
 function addTo() {
   var value = amount.value;
-  var newSpending = document.createElement("ol");
+  var newSpending = document.createElement("li");
   newSpending.innerHTML = value;
   newSpending.onclick = clear;
   spendings.appendChild(newSpending);
-  console.log(newSpending.length);
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  console.log(newSpending.reduce(reducer));
+  updateTotal()
 }
 
 function clear() {
   spendings.removeChild(this)
+  updateTotal()
+}
+
+function updateTotal() {
+  var totalSpend = 0
+  for (var index = 0; index < spendings.children.length; index++) {
+    var spending = spendings.children[index]
+    totalSpend += Number(spending.innerHTML)
+  }
+  total.innerHTML  = totalSpend
 }
