b014abeb0cc703f14dccf952520c2c7c2ce585c2
Taylor Hill <48519247+Thill799@users.noreply.github.com>
2019-04-25 16:52:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+var table = document.getElementById("myTable")
 var amount = document.getElementById("amount")
+var item = document.getElementById("item")
 var spendings = document.getElementById("spendings")
 var total = document.getElementById('total')
 
 function addTo() {
+  var row = table.insertRow();
+  var cell1 = row.insertCell();
+  var cell2 = row.insertCell();
+  cell1.innerHTML = document.getElementById("amount").value;
+  cell2.innerHTML = document.getElementById("item").value;  
   var value = amount.value;
   var newSpending = document.createElement("li");
   newSpending.innerHTML = value;
   newSpending.onclick = clear;
   spendings.appendChild(newSpending);
   updateTotal()
+  var item = document.createElement("li")
 }
 
 function clear() {
